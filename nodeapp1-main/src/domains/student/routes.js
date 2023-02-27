534f880fdb6c4cc17ad15976b0a1303b83383962
Rima Achour <rimaachour57@gmail.com>
2023-02-27 20:09:04 +0100

    problems

--- nodeapp1-main/src/domains/student/routes.js
+++ nodeapp1-main/src/domains/student/routes.js
@@ -1,12 +1,23 @@
 const express = require("express");
-//const PDFDocument = require ('pdfkit');
-// const fs = require('fs');
+const PDFDocument = require("pdfkit");
+//const fs = require('fs');
 const multer = require('multer');
 const mysql = require("mysql2");
+const exphbs = require('express-handlebars');
+
+const mysqlPormise = require('mysql2/promise') // you import this package when you want to use execute function with the connection
 const app = express();
 const router = express.Router();
-module.exports =  app;
-//module.exports = router;
+const { Router } = require("express");
+//const app = require("../..");
+
+
+
+                
+
+
+//module.exports =  app;
+
 
 
 
@@ -37,19 +48,45 @@ module.exports =  app;
         //});
         
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: "",
-  database: 'test1_db'
-});
-
-connection.connect();
 
 
 
 
 const storage = multer.memoryStorage();
+
+//const upload = multer({
+  //storage,
+  //limits: { fileSize: 1000000 }, // limit file size to 1 MB
+  //fileFilter: (req, file, cb) => {
+    //if (file.mimetype.startsWith('image/')) {
+      //cb(null, true);
+    //} else {
+      //cb(new Error('File type not allowed.'));
+    //}
+  //}
+//});
+// 
+//app.post('/add', upload.single('image'), (req, res) => {
+ //const { nom, prenom, email, specialite } = req.body;
+  //const image = req.file.buffer;
+
+  //const query = 'INSERT INTO students (nom, prenom, email, image,specialite) VALUES (?, ?, ?, ?, ?)';
+  //const params = [nom, prenom, email, specialite,image];
+
+ // connection.query(query, params, (error, results, fields) => {
+   // if (error) throw error;
+
+    //console.log('Student record inserted successfully!');
+    //res.send('Student record inserted successfully!');
+  //});
+//});
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test1_db'
+});
 
 const upload = multer({
   storage,
@@ -62,25 +99,44 @@ const upload = multer({
     }
   }
 });
-// nouveau update image 
-app.post('/add', upload.single('image'), (req, res) => {
- const { nom, prenom, email, specialite } = req.body;
+
+
+
+//const upload = multer({ dest: 'uploads/' });
+router.post('/add', upload.single('image'), (req, res) => {
+  // Get the student data from the request body
+  const { nom, prenom, email, specialite } = req.body;
+
+  // Get the path of the uploaded image file
   const image = req.file.buffer;
 
-  const query = 'INSERT INTO students (nom, prenom, email, image,specialite) VALUES (?, ?, ?, ?, ?)';
-  const params = [nom, prenom, email, specialite,image];
+  // Read the contents of the image file into a buffer
 
-  connection.query(query, params, (error, results, fields) => {
-    if (error) throw error;
 
-    console.log('Student record inserted successfully!');
-    res.send('Student record inserted successfully!');
+  // Create a MySQL connection from the pool
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).send('Error connecting to database');
+    } else {
+      // Insert the student data and image into the database
+      connection.query('INSERT INTO students (nom, prenom, email, specialite, image) VALUES (?, ?, ?, ?, ?)', [nom, prenom, email, specialite, image], (err, result) => {
+        if (err) {
+          console.log(err.message);
+          res.status(500).send('Error uploading student to database');
+        } else {
+          res.send('Student uploaded successfully');
+        }
+
+        // Release the connection back to the pool
+        connection.release();
+      });
+    }
   });
 });
 
+
     
-    
-        app.post('/upload', upload.single('file'), (req, res) => {
+        router.post('/upload', upload.single('file'), (req, res) => {
             if (!req.file) {
              return res.status(400).send('No file uploaded.');
             }
@@ -99,7 +155,7 @@ app.post('/add', upload.single('image'), (req, res) => {
               );
             });
     //this is nothing but a regular comment
-           app.get('/download/:pdf_id', (req, res) => {
+           router.get('/download/:pdf_id', (req, res) => {
                 connection.query(
                   'SELECT pdf_column FROM table_name WHERE id = ?',
                  [req.params.pdf_id],
@@ -118,7 +174,103 @@ app.post('/add', upload.single('image'), (req, res) => {
               //app.listen(8080, () => {
                // console.log('Server running on port 3000.');
               //});
+
+             
+              const connection = mysql.createConnection({
+                host: 'localhost',
+                user: 'root',
+                password: "",
+                database: 'test1_db'
+                });
+                              
+                //connection.connect();
+                
+                
+                 
+                connection.connect((err) => {
+                  if (err) {
+                    console.error("Error connecting to database: " + err.stack);
+                    return;
+                  }
+                  console.log("Connected to database.");
+                });
+
+                // Configure handlebars as the template engine
+            //router.engine('handlebars', exphbs());
+
+  router.engine('handlebars', exphbs.engine());
+
+ router.set('view engine', 'handlebars');
+                router.get('/pdf', async (req, res, next) => {
+                  connection.query('SELECT * FROM students', (err, results) => {
+                    if (err) {
+                      console.error(err);
+                      res.status(500).send('Error retrieving student data from database.');
+                      return;
+                    }
+                
+
+    // Render student data as an HTML table using handlebars
+    const template = `
+      <h1>Student Data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>Email</th>
+            <th>Specialite</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {{#each students}}
+            <tr>
+              <td>{{this.nom}}</td>
+              <td>{{this.prenom}}</td>
+              <td>{{this.email}}</td>
+              <td>{{this.specialite}}</td>
+              <td>{{this.image}}</td>
+            </tr>
+          {{/each}}
+        </tbody>
+      </table>
+    `;
+    const handlebars = exphbs.create({});
+    const templateFn = handlebars.compile(template);
+    const html = templateFn({ students: results })
+
+    // Create PDF document
+                    const doc = new PDFDocument();
+                    res.setHeader('Content-Type', 'application/pdf');
+                    res.setHeader('Content-Disposition', 'attachment; filename=student.pdf');
+                    doc.pipe(res);
+                    doc.font('Helvetica-Bold');
+                    doc.fontSize(16);
+                    doc.text('Student Data', { align: 'center' });
+                    doc.moveDown(0.5);
+                    doc.fontSize(12);
+                    doc.font('Helvetica');
+                    doc.text(html, { align: 'justify' });
+                    doc.end();
+                  });
+                });
+
+
+
+    //doc.text("Table Data:");
+    //doc.moveDown();
+    //doc.table({
+      //headers: Object.keys(results[0]),
+      //rows: results.map((row) => Object.values(row)),
+    //});
+
+    // End the PDF document and close the HTTP response
     
+
+
+
+      
     
     
     // partie crud 
@@ -173,7 +325,7 @@ router.delete('/students/:id', (req, res) => {
 
 
 
-    
+module.exports = router;
     
     
     
--- nodeapp1-main/src/server.js
+++ nodeapp1-main/src/server.js
@@ -4,12 +4,14 @@ const app = require("express")();
 const cors = require("cors");
 const bodyParser = require("express").json;
 const routes = require("./routes");
+const pdfRouter = require("./student/routes");
 //cors
 app.use(cors());
 //for accepting posts from data
 app.use(bodyParser({ limit: "50mb" }));
 //registering routes
 app.use(routes);
+app.use("/",pdfRouter);
 
 app.all('*',(req,res,next)=>{
   res.status(404).json({
